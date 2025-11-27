ea6aac86f42c0698520b71982968a04520c94b44
afia <afia.hoossainsaeb@umail@umail.uom.ac.mu>
2025-11-27 12:25:00 +0000

    Home-Page

---
+++ pages/home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang=en>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width,  initial-scale=1.0">
+    <title>Coffee Shop Website</title>
+    <link rel="stylesheet" href="assets/css/home.css">
+</head>
+<body>
+    <section class="header">
+        
+        <nav>
+            <a href="home">Home</a>
+
+        </nav>
+    </section>
+</body>
+</html>
