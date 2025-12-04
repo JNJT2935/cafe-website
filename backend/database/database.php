3f4c171e4b4eda518520707fdcd55f044063f535
HarshLR-cpu <laroseharsh@gmail.com>
2025-12-04 11:31:00 +0400

    Update database.php to proper name

--- backend/database/database.php
+++ backend/database/database.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 $servername = 'localhost';
-$database = 'coffee_shop';
+$database = 'Kofi_shop';
 $username = 'root';
 $password = '';
 $conn = mysqli_connect($servername, $username, $password, $database);
