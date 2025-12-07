bcd16bfecf4ca662e5f453e5b4178f2267d85114
bhargavgolam-cyber <bhargav.golam@umail.uom.ac.mu>
2025-12-07 10:17:15 +0400

    Add files via upload

---
+++ assets/includes/footer.php
@@ -0,0 +1,34 @@
+<link rel="stylesheet" href="assets/css/footer.css">
+<footer class="coffee-footer">
+    <div class="footer-inner">
+        <div class="footer-left">
+            <div class="footer-logo">
+                <a href="pages/Home_Page.php">
+                    <img src="assets/images/header_icon/coffee_logo.svg" alt="Coffee Shop Logo" class="logo-img">
+                    <span class="logo-text">COFFEE SHOP</span>
+                </a>
+            </div>
+        
+            <div class="footer-social">
+                <h2>Social Media</h2>
+                <div class="social-icons">
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-youtube"></i></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer-contact">
+            <h4>Contact Details</h4>
+            <p><span>Address:</span> Reduit Moka, University of Mauritius, Phase 2 Building</p>
+            <p><span>Phone number:</span> +1 202-918-2132</p>
+            <p><span>Email Address:</span> coffeeshop@gmail.com</p>
+            <p><span>Website:</span> www.coffeeshop.com</p>
+        </div>
+
+        <div class="footer-right">
+            <p>© 2025 Coffee Shop. All Rights Reserved.</p>
+        </div>
+    </div>
+</footer>
