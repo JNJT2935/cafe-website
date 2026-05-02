c0659171283a745d78b00ac89e2e2c0528ca49c9
bhavishp-byte <bhavish.poontaub@umail.uom.ac.mu>
2026-05-02 14:41:10 +0400

    Add files via upload

---
+++ backend/database/orders.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 29, 2026 at 02:33 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cafe`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
+  `customer_name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `address` text DEFAULT NULL,
+  `total` decimal(10,2) DEFAULT NULL,
+  `order_date` datetime DEFAULT NULL,
+  `status` varchar(20) DEFAULT NULL,
+  `delivery_note` text DEFAULT NULL,
+  `order_type` varchar(20) DEFAULT 'Delivery',
+  `items` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`order_id`, `customer_name`, `email`, `phone`, `address`, `total`, `order_date`, `status`, `delivery_note`, `order_type`, `items`) VALUES
+(1, 'John Doe', 'john@gmail.com', '57781122', 'Rose Hill', 505.00, '2025-12-01 00:00:00', 'Delivered', NULL, 'Delivery', 'Cappuccino x2, Coffee Latte x1'),
+(2, 'Emily', 'emily@yahoo.com', '57892233', 'Curepipe', 780.00, '2025-12-02 00:00:00', 'Pending', NULL, 'Delivery', 'Mocha x3, Croissant x2'),
+(3, 'Alex', 'alex@gmail.com', '57946209', 'Port Louis', 300.00, '2026-04-29 14:30:39', 'Cancelled', NULL, 'Delivery', 'Espresso x3');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
